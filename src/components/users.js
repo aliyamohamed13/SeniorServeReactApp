b08db40482ae0fc9d7729ca8ca45284088b8edde
Aliya <42981320+aliyamohamed13@users.noreply.github.com>
2020-03-11 22:40:46 -0700

    test loading users from spring api - working!

---
+++ src/components/users.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Users = ({ users }) => {
+  return (
+    <div>
+      <center>
+        <h1>User List</h1>
+      </center>
+      {users.map(user => (
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{user.username}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+              {user.firstName} {user.lastName}
+            </h6>
+            <p class="card-text">{user.address}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Users;
