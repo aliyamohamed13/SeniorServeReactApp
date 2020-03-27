3d2b2daae3adcbb26a045d6e243f1c2970c9bd85
Jonathan Wong <jonwong32@gmail.com>
2020-03-26 20:45:48 -0700

    beginning work with some filtering

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -2,11 +2,53 @@ import React, { Component } from "react";
 
 
 class Reviews extends Component {
-  render() {
-    return (
-    	<h1> Reviews Page </h1>
-    )
-  }
+	constructor() {
+		super()
+		this.state = {
+			reviewInfo: []
+		}
+	}
+
+	componentDidMount() {
+	 	console.log(this.props.username)
+		fetch("http://localhost:8080/api/v1/review/")
+		    .then(res => res.json())
+		    .then(data => {
+		       this.setState({ reviewInfo: data });
+		    })
+		    .catch(console.log);
+	}
+
+  	render() {
+	    return (
+	    	<div>
+		    	<form>
+		    		<h3>filter</h3>
+		    			  {['volunteerUserName', 'rating', 'taskID'].map(key => (
+						    <select key={key}>
+						      {this.state.reviewInfo.map(({ [key]: value }) => <option key={value}>{value}</option>)}
+						    </select>
+						  ))}
+		    	</form>
+
+		    
+		      <center>
+		        <h1>Review</h1>
+		      </center>
+		      {this.state.reviewInfo.map(review => (
+		        <div key={review.taskID} className="card">
+		          <div className="card-body">
+		            <h5 className="card-title">{review.description}</h5>
+		            <h6 className="card-subtitle mb-2 text-muted">
+		              Rating: {review.rating}
+		            </h6>
+		            <p className="card-text"> Volunteer: {review.volunteerUserName}</p>
+		          </div>
+		        </div>
+		      ))}
+		    </div>
+  		);
+  	}
 }
 
 export default Reviews;
