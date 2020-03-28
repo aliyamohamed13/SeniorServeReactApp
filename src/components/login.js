6dab00e08d3800e7a62679c3e3599ecfc5058af1
Aliya <42981320+aliyamohamed13@users.noreply.github.com>
2020-03-28 02:54:57 -0700

    ui change for login to use reactstrap

--- src/components/login.js
+++ src/components/login.js
@@ -3,89 +3,112 @@ import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import AppBar from "material-ui/AppBar";
 import RaisedButton from "material-ui/RaisedButton";
 import TextField from "material-ui/TextField";
+
+import { Container, Button, Form, FormGroup, Input } from "reactstrap";
+// import "../../index.css";
+import "../index.css";
+
 import Mainlanding from "./mainlanding.js";
 import axios from "axios";
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       username: "",
-      // password: ""
       redirect: false
-    }
+    };
   }
 
-  handleClick = (event) => {
+  handleClick(event) {
+    event.preventdefault();
     var apiBaseUrl = "http://localhost:8080/api/v1/user/";
     var self = this;
+    console.log("username: ${this.state.username}");
     console.log(this.state.username);
-    if(this.state.username === "") {
-      // do nothing
+    if (this.state.username === "") {
     } else {
-    // var payload = this.state.username;
-    //   password: this.state.password
-
-
-    //TODO this get doesnt send with payload, response seems to be just from baseurl
-    //work around to this would be
       axios
         .get(apiBaseUrl + this.state.username)
         .then(function(response) {
           console.log(apiBaseUrl);
           console.log(response);
           if (response.status === 200) {
-              if(response.data !== "") {
+            if (response.data !== "") {
               console.log("Login successful");
-              self.setState({redirect: true})
-              } else {
-            //TODO route to dashboard with user tasks sent as props
-
-            //   var taskBoard = [];
-            //   taskBoard.push(<TaskBoard appContext={self.props.appContext} />);
-            //   self.props.appContext.setState({
-            //     loginPage: [],
-            //     taskBoard: taskBoard
-            //   });
-            alert("Username does not exist, please register");
+              self.setState({ redirect: true });
+            } else {
+              alert("Username does not exist, please register");
             }
           } else {
             console.log("something went wrong");
             alert("Something went wrong, please try again");
-          }})
+          }
+        })
         .catch(function(error) {
           console.log(error);
-        })
-      }
+        });
+    }
   }
-
-    // axios({
-    //   method: "get",
-    //   url: "http://localhost:8080/api/v1/user/",
-    //   params: {
-    //   this.state.username,
-    //   })
-    //   .then()
-
   render() {
     if (this.state.redirect) {
       React.createContext(this.state.username);
-      console.log("redirecting...")
+      console.log("redirecting...");
       return (
         <Router>
           <Switch>
             <Route path="/mainlanding" component={Mainlanding} />
-            <Redirect to={{
-              pathname: "/mainlanding",
-              state: {username: this.state.username}
-              }}></Redirect>
+            <Redirect
+              to={{
+                pathname: "/mainlanding",
+                state: { username: this.state.username }
+              }}
+            ></Redirect>
           </Switch>
         </Router>
-      )
-    } 
+      );
+    }
     return (
-      <div>
+      <div className="LoginRegForm">
+        <Container>
+          <h1 style={{ marginBottom: "30px" }}>Login</h1>
+          <Form>
+            {/* onSubmit={event => this.handleClick(event)} */}
+            <FormGroup style={{ marginBottom: "30px" }}>
+              <Input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="username"
+                onChange={(event, newValue) =>
+                  this.setState({ username: newValue })
+                }
+              />
+            </FormGroup>
+            {/* <FormGroup style={{"marginBottom":"30px"}}>
+          <Input type="password" name="password" id="password" placeholder="password" />
+        </FormGroup> */}
+            <Button
+              style={{
+                marginBottom: "30px",
+                backgroundColor: "white",
+                color: "black"
+              }}
+              className="buttonOr"
+              onClick={event => this.handleClick(event)}
+            >
+              Submit
+            </Button>
+          </Form>
+        </Container>
+      </div>
+      /* <div>
         <MuiThemeProvider>
           <div>
             <AppBar title="Login" />
@@ -96,25 +119,25 @@ class Login extends Component {
                 this.setState({ username: newValue })
               }
             />
-            <br />
-            {/* <TextField
+            <br /> 
+       <TextField
               type="password"
               hintText="Enter your Password"
               floatingLabelText="Password"
               onChange={(event, newValue) =>
                 this.setState({ password: newValue })
               }
-            /> */}
-            <br />
+            /> 
+       <br />
             <RaisedButton
               label="Submit"
               primary={true}
               onClick={event => this.handleClick(event)}
             />
           </div>
-        </MuiThemeProvider>
-      </div>
-    )
+        </MuiThemeProvider> 
+      </div> */
+    );
   }
 }
 
