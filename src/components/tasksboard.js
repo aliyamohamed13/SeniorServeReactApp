c5407ac28b48435848f259e6c862e46ec71680c7
Jonathan Wong <jonwong32@gmail.com>
2020-03-28 21:11:07 -0700

    implement delete own tasks

--- src/components/tasksboard.js
+++ src/components/tasksboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Tasks from "./tasks";
+import axios from "axios";
 
 class TasksBoard extends Component {
 
@@ -20,7 +20,27 @@ class TasksBoard extends Component {
 	      .catch(console.log);
   	}
 
+  	handleDeleteTask = (taskID) => {
+  		console.log(taskID)
+  		var taskDeleteUrl = "http://localhost:8080/api/v1/task/task_ID=" + taskID
+  		axios.delete(taskDeleteUrl, {})
+  			 .then(response => {
+  			 	console.log("task deleted!")
+  			 	this.reset();
+  			 })
+  			 .catch(error => {
+  			 	console.log("there was problem deleting")
+  			 })
+  	}
 
+  	reset = () => {
+		axios.get("http://localhost:8080/api/v1/task/username=" + this.props.username)
+		     .then(res => {
+		     	console.log("reset!")
+		     	this.setState({ userTaskInfo: res.data });
+		     })
+		     .catch()
+  	}
 
 	render() {
 	  	
@@ -28,8 +48,24 @@ class TasksBoard extends Component {
 
 	    return (
 	    	<div>
-		    	<Tasks tasks={this.state.userTaskInfo} />
-		    </div>
+				<center>
+					<h1>Tasks List</h1>
+				</center>
+				{this.state.userTaskInfo.map(task => (
+					<div key={task.Task_ID} className="card">
+						<div className="card-body">
+							<h5 className="card-title">{task.Description}</h5>
+							<h6 className="card-subtitle mb-2 text-muted">
+								{task.Date} status: {task.Status}
+							</h6>
+							<p className="card-text">{task.Address} {task.PostalCode}</p>
+						<button type="button" onClick={() => this.handleDeleteTask(task.Task_ID)}>
+						Delete Task
+						</button>
+						</div>
+					</div>
+				))}
+			</div>
 	    )
 	}
 }
