3ae8a7993605702000d61f46b9d93ee47948036a
Aliya <42981320+aliyamohamed13@users.noreply.github.com>
2020-03-30 21:44:08 -0700

    attempting to show nav based on senior or not

--- src/components/mainlanding.js
+++ src/components/mainlanding.js
@@ -4,40 +4,99 @@ import Tasksboard from "./tasksboard.js";
 import Welcome from "./welcome.js";
 import Reviews from "./reviews.js";
 import Records from "./records.js";
+import NavSenior from "./navSenior.js";
 import GeneralTasksBoard from "./generaltasksboard";
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class Mainlanding extends Component {
-	constructor() {
-		super();
-		this.state = {
-			username: ""
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      senior: ""
+    };
+  }
 
-	componentDidMount() {
-		this.setState({username : this.props.location.state.username})	
-	}
+  componentDidMount() {
+    this.setState({ username: this.props.location.state.username });
+    fetch("http://localhost:8080/api/v1/user/senior/" + this.state.username)
+      .then(res => res.json())
+      .then(data => {
+        console.log("data is: " + data);
+        this.setState({ senior: data });
+      })
+      .catch(console.log);
+  }
 
-  	render() {
- 
- 		console.log(this.state.username)
-    	return (
-	    	<Router>
-	    		<div>
-					<Nav username = {this.state.username} />
-					<Switch>
-						<Route path="/mainlanding" component={Welcome} />
-			           	<Route path="/taskboard" render={() => <Tasksboard username = {this.state.username}/>} />
-			           	<Route path="/records" render={() => <Records username = {this.state.username}/>} />
-			           	<Route path="/reviews" render={() => <Reviews username = {this.state.username}/>} />
-			           	<Route path="/generalTasksBoard" render={() => <GeneralTasksBoard username = {this.state.username}/>} />
-		           	</Switch>
-	        	</div>
-	    	</Router>
-    	)
-  	}
+  //   checkBool(response, user) {
+  //     for (const i of response) {
+  //       if (i.username == user) {
+  //         return true;
+  //       }
+  //       return false;
+  //     }
+  //   }
+  //   isSenior(user) {
+  //     axios
+  //       .get("http://localhost:8080/api/v1/user/senior/")
+  //       .then(function(response) {
+  //         let returner = false;
+  //         console.log(response.data);
+  //         console.log("data is " + response.data);
+  //         for (const i of response.data) {
+  //           console.log("looping");
+  //           console.log(i.username);
+  //           console.log(user);
+  //           if (i.username == user) {
+  //             console.log("user is a senior");
+  //             returner = true;
+  //           }
+  //         }
+  //         console.log(returner);
+  //         return returner;
+  //       })
+  //       .catch(function(err) {
+  //         console.log(err);
+  //       });
+  //   }
+
+  render() {
+    // console.log(this.state.username);
+    // console.log(this.state.senior);
+    return (
+      <Router>
+        <div>
+          {this.state.senior ? (
+            <NavSenior username={this.state.username} />
+          ) : (
+            <Nav username={this.state.username} />
+          )}
+          <Switch>
+            <Route path="/mainlanding" component={Welcome} />
+            <Route
+              path="/taskboard"
+              render={() => <Tasksboard username={this.state.username} />}
+            />
+            <Route
+              path="/records"
+              render={() => <Records username={this.state.username} />}
+            />
+            <Route
+              path="/reviews"
+              render={() => <Reviews username={this.state.username} />}
+            />
+            <Route
+              path="/generalTasksBoard"
+              render={() => (
+                <GeneralTasksBoard username={this.state.username} />
+              )}
+            />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default Mainlanding;
---
+++ src/components/navSenior.js
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from "reactstrap";
+import "../index.css";
+
+const NavbarSenior = props => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand> Test</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-left" navbar>
+            <NavItem>
+              <Link to="/mainlanding">Home</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/taskboard">My Tasks</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/records"> Volunteer Records</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/reviews"> Reviews</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/generalTasksBoard">All Tasks</Link>
+            </NavItem>
+          </Nav>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <Link className="nav-link active" className="logout" href="/">
+                logout
+              </Link>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavbarSenior;
+
+// class NavbarSS extends Component {
+//   render() {
+//     return (
+//       <div>
+//         {/* {this.renderRedirect()} */}
+//         <Navbar className="change" dark expand="md">
+//           <NavbarBrand href="/home"> seniorServe</NavbarBrand>
+//           {/* <NavbarToggler onClick={this.toggle} /> */}
+//           <Collapse
+//             /* isOpen={this.state.isOpen}  */
+
+//             navbar
+//           >
+//             <Nav className="ml-left" navbar>
+//               <NavItem>
+//                 <NavLink href="/mainlanding"> Home</NavLink>
+//               </NavItem>
+//               <NavItem>
+//                 <NavLink href="/taskboard"> Taskboard</NavLink>
+//               </NavItem>
+//               <NavItem>
+//                 <NavLink href="/records"> Volunteer Records</NavLink>
+//               </NavItem>
+//               <NavItem>
+//                 <NavLink href="/reviews"> Reviews</NavLink>
+//               </NavItem>
+//             </Nav>
+//             <Nav className="ml-auto" navbar>
+//               <NavItem>
+//                 <NavLink
+//                   className="nav-link active"
+//                   className="logout"
+//                   href="/"
+//                 >
+//                   logout
+//                 </NavLink>
+//               </NavItem>
+//             </Nav>
+//           </Collapse>
+//         </Navbar>
+//       </div>
+
+//       //  render( props ) {
+//       //   	console.log(this.props)
+// //       //     return (
+//           	<ul>
+//           		<Link to="/mainlanding">
+//           			<li>Home</li>
+//           		</Link>
+//       			<Link to="/taskboard">
+//           			<li>Taskboard</li>
+//           		</Link>
+//           		<Link to="/records">
+//           			<li>Volunteer Records</li>
+//           		</Link>
+//           		<Link to="/reviews">
+//           			<li>Reviews</li>
+//           		</Link>
+//           	</ul>
+//     );
+//   }
+// }
+
+// export default NavbarSS;
