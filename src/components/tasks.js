4fe7477a1008113a697b8bb2ed074471d673e20c
Aliya <42981320+aliyamohamed13@users.noreply.github.com>
2020-04-05 18:40:19 -0700

    styling to cards on volunteer records page

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,21 +1,31 @@
 import React from "react";
+import { Container } from "reactstrap";
 
 const Tasks = ({ tasks }) => {
-  console.log(tasks)
+  console.log(tasks);
   return (
-    <div>
-      {tasks.map(task => (
+    <Container
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(4, 1fr)",
+        gridGap: "10px",
+        gridAutoRows: "minMax(100px, auto)",
+      }}
+    >
+      {tasks.map((task) => (
         <div key={task.Task_ID} className="card">
           <div className="card-body">
             <h5 className="card-title">{task.Description}</h5>
             <h6 className="card-subtitle mb-2 text-muted">
               Date: {task.Date} Status: {task.Status}
             </h6>
-            <p className="card-text">{task.Address} {task.City} {task.Province} {task.PostalCode}</p>
+            <p className="card-text">
+              {task.Address} {task.City} {task.Province} {task.PostalCode}
+            </p>
           </div>
         </div>
       ))}
-    </div>
+    </Container>
   );
 };
 
